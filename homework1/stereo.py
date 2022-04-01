39a0b7abbcf5d6e60072423866252f3d30e8f9a5
william ashbee <wsashbee@gmail.com>
2022-04-01 11:39:56 -0400

    finished hw code fps stereo

--- homework1/stereo.py
+++ homework1/stereo.py
@@ -18,12 +18,23 @@ monoRight = pipeline.create(dai.node.MonoCamera)
 depth = pipeline.create(dai.node.StereoDepth)
 xout = pipeline.create(dai.node.XLinkOut)
 
-xout.setStreamName("disparity")
+camRgb = pipeline.create(dai.node.ColorCamera)
+videoEnc = pipeline.create(dai.node.VideoEncoder)
+xoutRgb = pipeline.create(dai.node.XLinkOut)
 
-# Properties
-monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+
+#https://docs.luxonis.com/projects/api/en/latest/samples/VideoEncoder/rgb_mono_encoding/
+xout.setStreamName("disparity")
+xoutRgb.setStreamName("rgb")
+
+
+camRgb.setBoardSocket(dai.CameraBoardSocket.RGB)
+camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_4_K)
+videoEnc.setDefaultProfilePreset(camRgb.getFps(), dai.VideoEncoderProperties.Profile.MJPEG)
+
+monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_480_P)
 monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
-monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_480_P)
 monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
 
 # Create a node that will produce the depth map (using disparity output as it's easier to visualize depth this way)
@@ -39,17 +50,26 @@ monoLeft.out.link(depth.left)
 monoRight.out.link(depth.right)
 depth.disparity.link(xout.input)
 
+camRgb.video.link(xoutRgb.input)
+camRgb.video.link(videoEnc.input)
+
 # Connect to device and start pipeline
 with dai.Device(pipeline) as device:
 
     # Output queue will be used to get the disparity frames from the outputs defined above
     q = device.getOutputQueue(name="disparity", maxSize=4, blocking=False)
-
+    qRgb = device.getOutputQueue(name="rgb", maxSize=30, blocking=False)
+    
     while True:
+        print('fps rgb',camRgb.getFps())
+        print('fps Left',monoLeft.getFps())
+
         inDisparity = q.get()  # blocking call, will wait until a new data has arrived
         frame = inDisparity.getFrame()
         # Normalization for better visualization
         frame = (frame * (255 / depth.initialConfig.getMaxDisparity())).astype(np.uint8)
+        
+        print('disparity',frame.shape)
         
         cv2.imshow("disparity", frame)
 
@@ -59,5 +79,13 @@ with dai.Device(pipeline) as device:
         #horiz = np.concatenate((frame, frameDisparity,frameManip), axis=1)
         #cv2.imshow('all',horiz)
         
+        inRgb = qRgb.tryGet()  # Non-blocking call, will return a new data that has arrived or None otherwise
+        if inRgb is not None:
+            rgbframe= inRgb.getCvFrame()
+            print('rgbframe',rgbframe.shape)
+            
+            cv2.imshow("rgb", rgbframe)
+
+
         if cv2.waitKey(1) == ord('q'):
             break
