ed54aafc027a8fd59e4d1fc63756e98adf14a5bf
william ashbee <wsashbee@gmail.com>
2022-04-20 15:35:47 -0400

    integral image added

--- homework2/problem-6.py
+++ homework2/problem-6.py
@@ -1,5 +1,21 @@
 import cv2
 import depthai as dai
+import numpy as np
+
+def myIntegralImage(img):
+  
+  temp = np.pad(img, 1, 'constant', constant_values=0).astype(np.longlong)
+  #print(img.dtype)
+  #print(temp.dtype)
+  for j in range(1,temp.shape[0]-1):
+    for i in range(1,temp.shape[1]-1):
+      temp[j,i] = temp[j-1,i]+temp[j,i-1]+temp[j,i]-temp[j-1,i-1]
+
+
+  temp = (temp-np.min(temp))/(np.max(temp-np.min(temp)))*255
+  temp = temp.astype(img.dtype) 
+  return temp
+
 
 # Create pipeline
 pipeline = dai.Pipeline()
@@ -15,7 +31,7 @@ xoutRight.setStreamName('right')
 
 # Properties
 monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
-monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
+monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_480_P)
 monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
 monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
 
@@ -34,12 +50,18 @@ with dai.Device(pipeline) as device:
         # Instead of get (blocking), we use tryGet (non-blocking) which will return the available data or None otherwise
         inLeft = qLeft.tryGet()
         inRight = qRight.tryGet()
-
+        #print(inLeft.getCvFrame().shape)
         if inLeft is not None:
-            cv2.imshow("left", inLeft.getCvFrame())
+            iL = inLeft.getCvFrame()
+            mii = myIntegralImage(iL)
+            #print(mii,np.sum(iL))
+            #assert np.sum(iL) == mii[-2,-2]
+            #print(iL.shape)
+            cv2.imshow("left", iL)
+            cv2.imshow("integral image", myIntegralImage(iL))
 
-        if inRight is not None:
-            cv2.imshow("right", inRight.getCvFrame())
+        #if inRight is not None:
+            #cv2.imshow("right", inRight.getCvFrame())
 
         if cv2.waitKey(1) == ord('q'):
-            break
+            break   
