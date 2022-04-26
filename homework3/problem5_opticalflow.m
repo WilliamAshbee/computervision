510898357ad723c56b09320a6eb951f81565873a
william ashbee <wsashbee@gmail.com>
2022-04-26 17:49:11 -0400

    updated to skip frames

--- homework3/problem5_opticalflow.m
+++ homework3/problem5_opticalflow.m
@@ -1,8 +1,11 @@
-
+clear
 msg = "Choose your reference frame gap";
 opts = ["1" "11" "31"];
 choice = menu(msg,opts)
 vidReader = VideoReader('h3vid_shortend.avi');
+frames = vidReader.NumberOfFrames;
+vid = read(vidReader);
+
 opticFlow = opticalFlowHS;
 %opticFlowii = opticalFlowHS;
 %opticFlowiii = opticalFlowHS;
@@ -12,31 +15,25 @@ hViewPanel = uipanel(h,'Position',[0 0 1 1],'Title','Plot of Optical Flow Vector
 hPlot = axes(hViewPanel);
 
 hold on
-i = 1;
-frameRGB = readFrame(vidReader);
-frameGray = rgb2gray(frameRGB);
-referenceFrame = frameGray;
 if choice == 1
     gap = 1
 elseif choice == 2
     gap = 11
 else
-    gap == 31
+    gap = 31
 end
-while hasFrame(vidReader)
-    frameRGB = readFrame(vidReader);
-    if mod(i,gap) == 0
-        referenceFrame = frameGray;
-    end
-    
+
+for x = 1: floor(frames/gap)
+    frameRGB = vid(:, :, :, x*gap);
+    %referenceFrame = frameGray %no longer necessary since i'm skipping frames
     frameGray = rgb2gray(frameRGB);
     imshow(frameRGB)
     hold on
-    opticFlow.reset()
-    flow = estimateFlow(opticFlow,referenceFrame);
+    %opticFlow.reset() i used this when i wasn't skipping frames
+    %flow = estimateFlow(opticFlow,referenceFrame);now i'm just skipping
+    %the gap 
     flow = estimateFlow(opticFlow,frameGray);
     plot(flow,'DecimationFactor',[5 5],'ScaleFactor',200,'Parent',hPlot);
     hold off
-    pause(10^(-3))
-    i=i+1;
+    pause(gap/15)
 end
