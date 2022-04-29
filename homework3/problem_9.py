7f325bf9d0bef95a8daacb03b886bd23da923e49
william ashbee <wsashbee@gmail.com>
2022-04-29 15:43:05 -0400

    problem 9 done

--- homework3/problem_9.py
+++ homework3/problem_9.py
@@ -4,22 +4,39 @@ import cv2
 trained_face_data = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')#modified by will from stack overflow
 
 # Choose an image to detect faces in
-img = cv2.imread('p9.jpg')
+#########
+
+  
+# define a video capture object
+vid = cv2.VideoCapture(0)
+  
+while(True):
+      
+    # Capture the video frame
+    # by frame
+    ret, frame = vid.read()
+  
     
-# Must convert to greyscale
-grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-print(grayscaled_img.shape) 
-# Detect Faces 
-face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+    # Must convert to greyscale
+    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Detect Faces 
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
-#img_crop = []
 
-# Draw rectangles around the faces
-for (x, y, w, h) in face_coordinates:
-    cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
-    
-print(len(img_crop))
+    # Draw rectangles around the faces
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
 
-cv2.imshow('img',img)
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
 
-cv2.waitKey()
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+  
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
